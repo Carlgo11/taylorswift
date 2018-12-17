7d6c80a2f353dbb75da9733e72291957eacd5cf0
Carlgo11 <Carlgo11@users.noreply.github.com>
2018-12-17 23:51:02 +0100

    Fix bug where the script would try and play a directory instead of a file

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
 function getFiles($dir) {
+    // Check that dir exists and that ther are files in it
     if (file_exists($dir) && count($files = scandir($dir)) > 2) {
-        return $dir . "/" . $files[rand(0, (count($files) - 3))];
+      $file = $files[rand(0, (count($files) - 1))];
+      // Make sure the chosen file isn't a directory
+      if(!is_dir($file))
+        return $dir . '/' . $file;
+      else
+        getFiles($dir);
     } else {
         return null;
     }
@@ -14,12 +20,12 @@ function getFiles($dir) {
         <meta charset="UTF-8">
         <title>Taylor Swift</title>
     </head>
-    <body style="background-image: url('<?php echo getFiles("images"); ?>'); background-repeat: no-repeat; background-size: cover">
+    <body style="background-image: url('<?php echo getFiles('images') ?>'); background-repeat: no-repeat; background-size: cover">
         <div>
             <input type="range" id="inputvolume" onchange="setVolume()" min="0" max="10">
         </div>
         <audio autoplay="" id="audioelement">
-            <source src="<?php echo getFiles("songs"); ?>">
+            <source src="<?php echo getFiles('songs') ?>">
         </audio>
         <script>
             var song = document.getElementById("audioelement");
